5abcb9fff670ecffccb05578e6b0fc7b48b1ab6d
Rickard Östergård <rickard.ostergard@gmail.com>
2020-10-28 12:10:34 +0100

    fixed typo

--- restore.sh
+++ restore.sh
@@ -27,4 +27,4 @@ FETCH_DIR=$(mktemp -d)
 gsutil cp gs://$GCS_BUCKET/$GCS_BUCKET_PATH/$FILE_NAME $FETCH_DIR
 tar -zxf $FETCH_DIR/keycloak-20201028_102517.tar.gz -C $RESTORE_SRC_DIR --strip-components=1
 rm -rf $FETCH_DIR
-echo "Backup $FILE_NAME is now is availbe in $RESTORE_SRC_DIR"
+echo "Backup $FILE_NAME is now is available in $RESTORE_SRC_DIR"
